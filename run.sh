0977747709efc8994e8887c7958910513460797e
Ajdin Trejic <ajdintrejic@gmail.com>
2025-01-19 20:54:53 +0100

    add common.c to run.sh, fix params

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ CC=/opt/homebrew/opt/llvm/bin/clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c
+    kernel.c common.c
 
-$QEMU -machine virt -bios default -serial mon:stdio --no-reboot \
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
     -kernel kernel.elf
 
