998b0cbc0bc1533ef852378ac8813b29cc76e7fc
Ajdin Trejic <ajdintrejic@gmail.com>
2025-01-19 20:54:29 +0100

    and then there was ram

--- kernel.c
+++ kernel.c
@@ -8,6 +8,20 @@
     } while (0)
 
 extern char __bss[], __bss_end[], __stack_top[];
+extern char __free_ram[], __free_ram_end[];
+
+paddr_t alloc_pages(uint32_t n) {
+    static paddr_t next_paddr = (paddr_t) __free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t) __free_ram_end)
+        PANIC("out of memory");
+
+    memset((void *) paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
+
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
                        long arg5, long fid, long eid) {
@@ -143,6 +157,12 @@ void kernel_main(void) {
 
     //PANIC("booted!");
     //printf("unreachable here!\n");
+
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("alloc_pages test: paddr0=%x\n", paddr0);
+    printf("alloc_pages test: paddr1=%x\n", paddr1);
+
 
     for (;;) {
         __asm__ __volatile__("wfi");
